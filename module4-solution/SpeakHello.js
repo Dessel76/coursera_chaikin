789767cabb341df0740b5f3e84b1fa2ef967dd2d
semushindv@outlook.com <semushindv@outlook.com>
2018-11-14 14:59:49 +0500

    module4-solution

---
+++ module4-solution/SpeakHello.js
@@ -0,0 +1,11 @@
+(function(window){
+	helloSpeaker = {};
+	// DO NOT attach the speakWord variable to the 'helloSpeaker' object.
+	var speakWord = "Hello";
+
+	helloSpeaker.speak = function speak(name) {
+	  console.log(speakWord + " " + name);
+	}
+
+	window.helloSpeaker = helloSpeaker;
+})(window);
